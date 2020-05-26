8e628c5a2bcd61463bb030ee27c9c37dc692b5a6
xiaoxzhang <xiaoxzhang@163.com>
2020-05-26 22:37:39 +0800

    one

---
+++ src/main/java/com/lm/lmexamination/LmexaminationApplication.java
@@ -0,0 +1,13 @@
+package com.lm.lmexamination;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmexaminationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmexaminationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lm/lmexamination/LmexaminationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lm.lmexamination;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmexaminationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
